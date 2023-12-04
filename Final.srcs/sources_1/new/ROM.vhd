a705121f89f6d6953c636860d07005f5854f5303
Astoner <aidanstoner1@gmail.com>
2023-12-04 18:17:38 -0500

    coded ROM need to test

--- Final.srcs/sources_1/new/ROM.vhd
+++ Final.srcs/sources_1/new/ROM.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,12 +32,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity ROM is
---  Port ( );
+    Port (address : in std_logic_vector(0 to 35 );
+        dataOut : out std_logic_vector(0 to 2));
 end ROM;
 
 architecture Behavioral of ROM is
+type ROM_type is array(0 to 35) of std_logic_vector(0 to 2);
+signal ROMarray : ROM_type;
 
 begin
-
+    ROMarray(1) <= "001";
+    ROMarray(2) <= "010";
+    dataOut <= ROMarray(to_integer(unsigned(address)));
 
 end Behavioral;

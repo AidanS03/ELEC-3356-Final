49a7856a23fc5d42b4c1f61c8da267d73590fb08
Astoner <aidanstoner1@gmail.com>
2023-12-04 18:04:59 -0500

    RAM has been tested and is working

--- Final.srcs/sim_1/new/RAM_Test.vhd
+++ Final.srcs/sim_1/new/RAM_Test.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 12/04/2023 01:36:33 PM
+-- Create Date: 12/04/2023 05:39:19 PM
 -- Design Name: 
--- Module Name: RAM - Behavioral
+-- Module Name: RAM_Test - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -24,34 +24,54 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
+--use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity RAM is
+entity RAM_Test is
+--  Port ( );
+end RAM_Test;
+
+architecture Behavioral of RAM_Test is
+component RAM is
     Port (address : in std_logic_vector(0 to 5); 
         dataIn : in std_logic_vector(0 to 2);
         writeEN : in std_logic;
         clk : in std_logic;
         dataOut : out std_logic_vector(0 to 2));
-end RAM;
-
-architecture Behavioral of RAM is
-type RAM_type is array(0 to 35) of std_logic_vector(0 to 2);
-signal RAMarray : RAM_type;
-
+end component;
+signal address : std_logic_vector(0 to 5);
+signal dataIn, dataOut : std_logic_vector(0 to 2);
+signal writeEn, clk : std_logic;
 begin
-process(clk)
+tb : RAM port map(address => address,
+    dataIn => dataIn,
+    writeEn => writeEN,
+    clk => clk,
+    dataOut => dataOut);
+process
 begin
-    if rising_edge(clk) then
-        if writeEn = '1' then 
-            RAMarray(to_integer(unsigned(address))) <= dataIn;
-        end if;
-    end if;
+    clk <= '0';
+    writeEn <= '0';
+    wait for 100ns;
+    
+    address <= "000000";
+    dataIn <= "101";
+    clk <= '1';
+    wait for 100ns;
+    
+    clk <= '0';
+    wait for 100ns;
+    
+    address <= "000000";
+    dataIn <= "101";
+    writeEn <= '1';
+    clk <= '1';
+    wait;
+        
 end process;
-dataOut <= RAMarray(to_integer(unsigned(address)));
 
 end Behavioral;
--- Final.srcs/sources_1/new/ROM.vhd
+++ Final.srcs/sources_1/new/ROM.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 12/04/2023 01:36:33 PM
+-- Create Date: 12/04/2023 05:38:48 PM
 -- Design Name: 
--- Module Name: RAM - Behavioral
+-- Module Name: ROM - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -24,34 +24,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
+--use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity RAM is
-    Port (address : in std_logic_vector(0 to 5); 
-        dataIn : in std_logic_vector(0 to 2);
-        writeEN : in std_logic;
-        clk : in std_logic;
-        dataOut : out std_logic_vector(0 to 2));
-end RAM;
+entity ROM is
+--  Port ( );
+end ROM;
 
-architecture Behavioral of RAM is
-type RAM_type is array(0 to 35) of std_logic_vector(0 to 2);
-signal RAMarray : RAM_type;
+architecture Behavioral of ROM is
 
 begin
-process(clk)
-begin
-    if rising_edge(clk) then
-        if writeEn = '1' then 
-            RAMarray(to_integer(unsigned(address))) <= dataIn;
-        end if;
-    end if;
-end process;
-dataOut <= RAMarray(to_integer(unsigned(address)));
+
 
 end Behavioral;
